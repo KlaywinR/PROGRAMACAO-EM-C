dc0fab1c4e3b8856ae3baf963e185a68e374cc96
Klaywin Ryan <klaywindias@gmail.com>
2025-05-01 09:22:29 -0300

    Média Aritmética
     O programa recebe o valor de 3 notas entre 0 e 100 de um determinado discente e exibe o valor da media aritmética.

---
+++ questao04.c
@@ -0,0 +1,35 @@
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){ 
+    int nota1, nota2, nota3;
+    printf("DIGITE A PRIMEIRA NOTA DO DISCENTE --->");
+    scanf("%i", &nota1); 
+
+    printf("DIGITE A SEGUNDA NOTA DO DISCENTE --->");
+    scanf("%i", &nota2);
+
+    printf("DIGITTE A TERCEIRA NOTA DO DISCENTE --->");
+    scanf("%i", &nota3);
+
+float media =   (nota1 + nota2 + nota3) /3;
+
+printf("\n media das notas: %.2f",  round (media));
+
+////informando se foi aprovado ou nao 
+//aprovado é maior que sete (7.0)
+
+
+
+
+
+return 0;
+
+
+}
+
+
+
+
