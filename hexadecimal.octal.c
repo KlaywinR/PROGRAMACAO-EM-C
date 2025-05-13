afa30b53901b08f1aa6a6a5fbb50e979eec6d353
Klaywin Ryan A. Dias <klaywindias@gmail.com>
2025-05-13 14:09:09 -0300

    Hexadecimal e Octadecimal.
    Este programa em C converte um número inteiro fornecido pelo usuário em suas representações hexadecimal e octal. Ele solicita um número decimal como entrada, realiza as conversões e exibe os resultados.

---
+++ hexadecimal.octal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void){
+ 
+     int numero_dado;
+      
+     printf("\n\n--DIGITE AQUI UM  NUMERO DECIMAL--\n\n");
+     scanf("%d", &numero_dado);
+
+    printf("Hexadecimal: %X\n", numero_dado);
+    printf("Octal: %o\n", &numero_dado);
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
